6eedd2d6719e231c0770a671e05fd83565b3866c
Majormyles <majormilesvigour@gmail.com>
2025-03-01 11:00:32 +0000

    updated

--- frontend/src/components/CallToAction.js
+++ frontend/src/components/CallToAction.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './css/CallToAction.css';
-
+import { Link, NavLink } from 'react-router-dom';
+import jobsearchImage from '../assets/images/woman.png';
 const CallToAction = () => {
   return (
     <div className="cta-container">
       <div className="cta-content">
-        <h1 className="cta-title">Create A Better Future For Yourself</h1>
+        <h1 className="cta-title">Build Your Path to a Brighter Tomorrow</h1>
         <p className="cta-description">
-          At eu lobortis pretium tincidunt amet lacus ut aenean aliquet. 
-          Blandit a massa elementum id scelerisque rhoncus...
+          Discover opportunities that align with your skills and aspirations.
+          Take the next step in your career journey and unlock your full potential.
         </p>
-        <button className="cta-button">Search Job</button>
+        <Link to='/jobs'><button className="cta-button">Find Jobs</button></Link>
       </div>
       <div className="cta-image-container">
         {/* This div will have the background image with blur effect applied in CSS */}
+        <img src={jobsearchImage} alt="Company hero" className="hero-image" />
       </div>
     </div>
   );
