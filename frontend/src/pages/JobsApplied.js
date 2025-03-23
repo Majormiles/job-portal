490bf7bc632f5271eae270477d7ed11361071d87
Majormyles <majormilesvigour@gmail.com>
2025-03-23 05:44:17 +0000

    updated

--- frontend/src/pages/JobsApplied.js
+++ frontend/src/pages/JobsApplied.js
@@ -4,13 +4,14 @@ import JobsApplied from '../components/AppliedJobs';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Sidebar from '../components/Sidebar';
+import '../components/css/AppliedJobs.css'; // Make sure to create this CSS file with the styles from above
 
 const JobListingsPage = () => {
   return (
-    <div className="page-container">
+    <div className="page-container-appliedjobs">
       <Header />
       <div className="hero"></div>
-      <Sidebar />
+      <Sidebar className="sidebar" />
       <div className="content-section">
         <JobsApplied />
       </div>
